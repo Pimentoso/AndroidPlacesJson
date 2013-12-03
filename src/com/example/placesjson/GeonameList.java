b97234dbd43bb9c06c3d424ec8ed2e6595fedf25
Michele Ferri <zomgsako@gmail.com>
2013-12-03 21:26:13 +0100

    Initial commit

---
+++ src/com/example/placesjson/GeonameList.java
@@ -0,0 +1,15 @@
+package com.example.placesjson;
+
+import java.util.List;
+
+public class GeonameList {
+	private List<Geonames> geonames;
+
+	public List<Geonames> getGeonames() {
+		return this.geonames;
+	}
+
+	public void setGeonames(List<Geonames> geonames) {
+		this.geonames = geonames;
+	}
+}
